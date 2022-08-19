9c5967b29270129cc3ef090f25ab57ca02874bfd
arakaki_seina <87635517+anies1212@users.noreply.github.com>
2022-08-19 15:15:48 +0900

    Initial Commit

---
+++ SourceryTutorialAppUITests/SourceryTutorialAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SourceryTutorialAppUITestsLaunchTests.swift
+//  SourceryTutorialAppUITests
+//
+//  Created by 新垣 清奈 on 2022/08/19.
+//
+
+import XCTest
+
+class SourceryTutorialAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
